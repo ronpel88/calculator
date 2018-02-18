93ddba28bb0f1fc9c543230eb5f2f3a20ce4b5a6
Ron Shaiko <Ron.Shaiko@amdocs.com>
2018-02-18 15:42:43 +0200

    changed from choice to operators added documentation

--- calculator.py
+++ calculator.py
@@ -1,58 +1,66 @@
 import math
 
-
+# get_calc_options function return a string which contain all calc options (option in each line)
 def get_calc_options():
     options = """
-    1. Add
-    2. Subtract
-    3. Multiply
-    4. Divide
-    5. Modulo
-    6. PI
-    7. Help
+    1. Add   +
+    2. Subtract   -
+    3. Multiply   * 
+    4. Divide   /
+    5. Modulo   %
+    6. PI   pi
+    7. Help   usage
     """
     return options
 
+# usage function return a string which contain calc usage
 def usage():
     calc_usage = """calculator support the following operators:"""
     calc_usage = calc_usage + get_calc_options()
     return calc_usage
 
+# calculate function get as parameters operation (like +/-/...) and two numbers and return a string with result
+def calculate(num1, operation, num2):
+    # print "num1= " + str(num1)
+    # print "operation= " + str(operation)
+    # print "num2= " + str(num2)
 
-def calculate(operation, num1, num2):
-    print "choice= " + str(operation)
-    print "num1= " + str(num1)
-    print "num2= " + str(num2)
-
-    if operation == 1:
+    if operation == "+":
         return str(num1) + "+" + str(num2) + "=" + str(num1+num2)
-    elif operation == 2:
+    elif operation == "-":
         return str(num1) + "-" + str(num2) + "=" + str(num1-num2)
-    elif operation == 3:
+    elif operation == "*":
         return str(num1) + "*" + str(num2) + "=" + str(num1*num2)
-    elif operation == 4:
+    elif operation == "/":
         return str(num1) + "/" + str(num2) + "=" + str(num1/num2)
-    elif operation == 5:
+    elif operation == "%":
         return str(num1) + "%" + str(num2) + "=" + str(num1%num2)
     else:
         return "Invalid input"
 
 
+# main function - running first
 if __name__ == '__main__':
+    # display operation options to user
     print "Select operation:"
     print get_calc_options()
 
+    # running with loop to be able to get inputs over and over
     while True:
-        # Take input from the user
-        operation = input("Please enter choice:")
-        if operation == 6:
+        # get input from the user - operation
+        operation = raw_input("Please enter operation:")
+        if operation == "pi":
             result = math.pi
-        elif operation == 7:
+        elif operation == "usage":
             result = usage()
         else:
+            # get input from the user - num1 and num2
             num1 = input("Enter first number: ")
             num2 = input("Enter second number: ")
+            # convert num1 and num2 to numerical values, as you cannot do math on strings
             int_num1 = int(num1)
             int_num2 = int(num2)
-            result = calculate(operation, int_num1, int_num2)
+            # calculate the math and return result that contain string like: 1+1=2
+            result = calculate(int_num1, operation, int_num2)
+        # print result
         print result
